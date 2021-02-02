901cecaf5b1f6a528215089fb1b2e7ede44df26d
trystoh <53058450+trystoh@users.noreply.github.com>
2021-02-02 13:37:00 -0800

    composition for sllist stack

---
+++ linked_lists/stack_sll.py
@@ -0,0 +1,37 @@
+from SLList import *
+
+class StackSLL:
+
+	def __init__(self):
+
+		self.sll = SinglyLinkedList()
+
+	def head(self):
+
+		return self.sll.head
+
+	def tail(self):
+
+		return self.sll.tail
+
+	def push(self, val):
+
+		self.sll.add(0, val)
+
+	def pop(self):
+
+		return self.sll.remove(0)
+
+if __name__ == "__main__":
+
+	stack = StackSLL()
+
+	for i in range(10):
+		stack.push(i)
+
+	print(stack.sll)
+	print(stack.sll.size)
+	for i in range(10):
+		print(stack.pop().val)
+		print(stack.sll)
+		print(stack.sll.size)
